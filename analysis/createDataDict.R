16c2b6944f13f1a9fb94c343f9ab2dcba09c85f1
Juan <you@example.com>
2025-12-04 21:48:46 -0300

    reorg

--- analysis/createDataDict.R
+++ analysis/createDataDict.R
@@ -7,10 +7,8 @@ out <- list()
 for (f in files) {
   # Read the raw header line exactly as-is
   header_raw <- readLines(f, n = 1)
-  
   # Split by comma without trimming anything
   cols <- strsplit(header_raw, ",", fixed = TRUE)[[1]]
-  
   for (c in cols) {
     out[[length(out) + 1]] <- list(
       "@type" = "PropertyValue",
